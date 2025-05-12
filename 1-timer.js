86007c0ef6122a4b35d966f3fbc00bdb5ae749d5
IhorShcherbak <157756063+IhorShcherbak@users.noreply.github.com>
2025-05-12 00:03:57 +0000

    Deploying to gh-pages from @ IhorShcherbak/goit-js-hw-10@cdc3dd7299e9fd0e1d1f1c1c351fa10c243ca25e 🚀

--- 1-timer.js
+++ 1-timer.js
@@ -1,25 +1,2 @@
-import"./assets/styles-B4zVjhdP.js";import{f as m,i as f}from"./assets/vendor-BbbuE1sJ.js";document.body.innerHTML=`
-  <div class="picker-container">
-    <input type="text" id="datetime-picker"/>
-    <button type="button" data-start disabled>Start</button>
-  </div>
-  <div class="timer">
-    <div class="field">
-      <span class="value" data-days>00</span>
-      <span class="label">Days</span>
-    </div>
-    <div class="field">
-      <span class="value" data-hours>00</span>
-      <span class="label">Hours</span>
-    </div>
-    <div class="field">
-      <span class="value" data-minutes>00</span>
-      <span class="label">Minutes</span>
-    </div>
-    <div class="field">
-      <span class="value" data-seconds>00</span>
-      <span class="label">Seconds</span>
-    </div>
-  </div>
-`;let o=null,i=null;const e={input:document.getElementById("datetime-picker"),startBtn:document.querySelector("[data-start]"),days:document.querySelector("[data-days]"),hours:document.querySelector("[data-hours]"),minutes:document.querySelector("[data-minutes]"),seconds:document.querySelector("[data-seconds]")},y={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){const s=t[0];s<=new Date?(f.warning({title:"Warning",message:"Please choose a date in the future",position:"topRight"}),e.startBtn.disabled=!0):(o=s,e.startBtn.disabled=!1)}};m(e.input,y);e.startBtn.addEventListener("click",()=>{o&&(e.startBtn.disabled=!0,e.input.disabled=!0,i=setInterval(()=>{const s=o-new Date;if(s<=0){clearInterval(i),r({days:0,hours:0,minutes:0,seconds:0}),e.input.disabled=!1;return}const a=v(s);r(a)},1e3))});function r({days:t,hours:s,minutes:a,seconds:d}){e.days.textContent=n(t),e.hours.textContent=n(s),e.minutes.textContent=n(a),e.seconds.textContent=n(d)}function n(t){return String(t).padStart(2,"0")}function v(t){const l=Math.floor(t/864e5),u=Math.floor(t%864e5/36e5),c=Math.floor(t%864e5%36e5/6e4),p=Math.floor(t%864e5%36e5%6e4/1e3);return{days:l,hours:u,minutes:c,seconds:p}}
+import"./assets/styles-B4zVjhdP.js";import{f,i as h}from"./assets/vendor-BbbuE1sJ.js";let a=null,d=null;const e={input:document.getElementById("datetime-picker"),startBtn:document.querySelector("[data-start]"),days:document.querySelector("[data-days]"),hours:document.querySelector("[data-hours]"),minutes:document.querySelector("[data-minutes]"),seconds:document.querySelector("[data-seconds]")},y={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){const n=t[0];n<=new Date?(h.warning({title:"Warning",message:"Please choose a date in the future",position:"topRight"}),e.startBtn.disabled=!0):(a=n,e.startBtn.disabled=!1)}};f(e.input,y);e.startBtn.addEventListener("click",()=>{a&&(e.startBtn.disabled=!0,e.input.disabled=!0,d=setInterval(()=>{const n=a-new Date;if(n<=0){clearInterval(d),u({days:0,hours:0,minutes:0,seconds:0}),e.input.disabled=!1;return}const o=p(n);u(o)},1e3))});function u({days:t,hours:n,minutes:o,seconds:r}){e.days.textContent=s(t),e.hours.textContent=s(n),e.minutes.textContent=s(o),e.seconds.textContent=s(r)}function s(t){return String(t).padStart(2,"0")}function p(t){const i=Math.floor(t/864e5),c=Math.floor(t%864e5/36e5),l=Math.floor(t%864e5%36e5/6e4),m=Math.floor(t%864e5%36e5%6e4/1e3);return{days:i,hours:c,minutes:l,seconds:m}}
 //# sourceMappingURL=1-timer.js.map
--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,25 +1,2 @@
-import"./assets/styles-B4zVjhdP.js";import{i as l}from"./assets/vendor-BbbuE1sJ.js";const a=`
-  <form class="form">
-    <label>
-      Delay (ms)
-      <input type="number" name="delay" required />
-    </label>
-
-    <fieldset>
-  <legend>State</legend>
-  <div class="radio-group">
-    <label>
-      <input type="radio" name="state" value="fulfilled" required />
-      Fulfilled
-    </label>
-    <label>
-      <input type="radio" name="state" value="rejected" required />
-      Rejected
-    </label>
-  </div>
-</fieldset>
-
-    <button type="submit">Create notification</button>
-  </form>
-`;document.body.insertAdjacentHTML("beforeend",a);const t=document.querySelector(".form");t.addEventListener("submit",s=>{s.preventDefault();const i=Number(t.elements.delay.value),o=t.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{o==="fulfilled"?e(i):r(i)},i)}).then(e=>{l.success({title:"✅",message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{l.error({title:"❌",message:`Rejected promise in ${e}ms`,position:"topRight"})}),t.reset()});
+import"./assets/styles-B4zVjhdP.js";import{i}from"./assets/vendor-BbbuE1sJ.js";const t=document.querySelector(".form");t.addEventListener("submit",o=>{o.preventDefault();const s=Number(t.elements.delay.value),m=t.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{m==="fulfilled"?e(s):r(s)},s)}).then(e=>{i.success({title:"✅",message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"❌",message:`Rejected promise in ${e}ms`,position:"topRight"})}),t.reset()});
 //# sourceMappingURL=2-snackbar.js.map
